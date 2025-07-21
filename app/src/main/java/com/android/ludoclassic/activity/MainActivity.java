1be4c4d34a2e1f64fdc7446f44279740112ed840
Miah Mohammed Yeasin <miahmohammedyeasin@gmail.com>
2025-07-21 15:47:52 +0600

    v8

--- app/src/main/java/com/android/ludoclassic/activity/MainActivity.java
+++ app/src/main/java/com/android/ludoclassic/activity/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.ludoclassic.R;
+import com.srtech.ludoclassic.R;
 
 public class MainActivity extends AppCompatActivity {
     @Override
